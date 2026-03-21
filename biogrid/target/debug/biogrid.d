93f4ac977547a8a10b16fd08b77d5cb6f4b4fd48
Josiah Mbao <your-email@example.com>
2026-03-21 11:05:06 +0300

    remove build artifacts and update gitignore

--- biogrid/target/debug/biogrid.d
+++
@@ -1,1 +0,0 @@
-/Users/josiah/LocalProjects/BioGrid/BioGrid/target/debug/biogrid: /Users/josiah/LocalProjects/BioGrid/BioGrid/src/main.rs
--- biogrid/target/debug/deps/biogrid-69643050d52fff32.d
+++
@@ -1,5 +0,0 @@
-/Users/josiah/LocalProjects/BioGrid/BioGrid/target/debug/deps/biogrid-69643050d52fff32.d: src/main.rs
-
-/Users/josiah/LocalProjects/BioGrid/BioGrid/target/debug/deps/biogrid-69643050d52fff32: src/main.rs
-
-src/main.rs:
